bbcef0f68dbd56bf6e70f152f8bbca731286e3fc
Ioannis Koinaris (ext.) <Ioannis.Koinaris@adesson.de>
2021-10-19 09:14:56 +0200

    First Commit

---
+++ rest-number/src/test/java/org/ikoinaris/microservices/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.ikoinaris.microservices;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends NumberResourceTest {
+
+    // Execute the same tests but in native mode.
+}
