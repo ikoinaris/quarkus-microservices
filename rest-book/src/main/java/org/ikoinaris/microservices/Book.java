742dd9c42c789ce6165689c05f14d38bdbc4f5cb
Ioannis Koinaris (ext.) <Ioannis.Koinaris@adesson.de>
2021-10-19 11:18:00 +0200

    JSON output customized

--- rest-book/src/main/java/org/ikoinaris/microservices/Book.java
+++ rest-book/src/main/java/org/ikoinaris/microservices/Book.java
@@ -1,14 +1,20 @@
 package org.ikoinaris.microservices;
 
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.json.bind.annotation.JsonbProperty;
 import java.time.Instant;
 
 public class Book {
 
+    @JsonbProperty("isbn13")
     public String isbn13;
     public String title;
     public String author;
+    @JsonbProperty("year_Of_Publication")
     public int yearOfPublication;
     public String genre;
+    @JsonbDateFormat("yyyy/MM/dd")
+    @JsonbProperty("creation_Date")
     public Instant creationDate;
 
     @Override
