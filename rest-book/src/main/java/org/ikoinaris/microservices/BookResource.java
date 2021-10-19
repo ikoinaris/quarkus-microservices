8f6f1e59570a32de51638aa2a55d3d57b3ad833f
Ioannis Koinaris (ext.) <Ioannis.Koinaris@adesson.de>
2021-10-19 11:13:48 +0200

    Book class created and injected in main class

--- rest-book/src/main/java/org/ikoinaris/microservices/BookResource.java
+++ rest-book/src/main/java/org/ikoinaris/microservices/BookResource.java
@@ -1,16 +1,27 @@
 package org.ikoinaris.microservices;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.time.Instant;
 
 @Path("/api/books")
 public class BookResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTeasy";
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createBook(@FormParam("title") String title,
+                               @FormParam("author") String author,
+                               @FormParam("year") int yearOfPublication,
+                               @FormParam("genre") String genre) {
+        Book book = new Book();
+        book.isbn13 = "We get it from Number Microservice";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.creationDate = Instant.now();
+        book.genre = genre;
+        return Response.status(201).entity(book).build();
     }
 }
