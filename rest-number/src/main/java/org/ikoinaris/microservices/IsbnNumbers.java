0d4d13271fa87775093bffd08610cc852d909a70
Ioannis Koinaris (ext.) <Ioannis.Koinaris@adesson.de>
2021-10-19 10:00:06 +0200

    JSON output customized

--- rest-number/src/main/java/org/ikoinaris/microservices/IsbnNumbers.java
+++ rest-number/src/main/java/org/ikoinaris/microservices/IsbnNumbers.java
@@ -1,12 +1,16 @@
 package org.ikoinaris.microservices;
 
+import javax.json.bind.annotation.JsonbProperty;
+import javax.json.bind.annotation.JsonbTransient;
 import java.time.Instant;
 
 public class IsbnNumbers {
 
+    @JsonbProperty("isbn_13")
     public String isbn13;
+    @JsonbProperty("isbn_10")
     public String isbn10;
-
+    @JsonbTransient
     public Instant generationDate;
 
     @Override
